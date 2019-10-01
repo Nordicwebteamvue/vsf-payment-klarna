d0ed1524923ffe07611b29349162797b88a0324e
Max Malm <benjick@dumfan.net>
2019-10-01 15:24:53 +0200

    Rename webpack chunk (#50)

--- frontend/payment-klarna/hooks/beforeRegistration.ts
+++ frontend/payment-klarna/hooks/beforeRegistration.ts
@@ -1,7 +1,7 @@
 import { router } from '@vue-storefront/core/app'
 import { RouterManager } from '@vue-storefront/core/lib/router-manager'
 import { setupMultistoreRoutes } from '@vue-storefront/core/lib/multistore'
-const Confirmation = () => import(/* webpackChunkName: "vsf-klarna-confirmation" */ '../components/Confirmation.vue')
+const Confirmation = () => import(/* webpackChunkName: "vsf-payment-klarna" */ '../components/Confirmation.vue')
 import { PayPalKcoRoutes } from '../pages/routes'
 
 export function beforeRegistration({ Vue, store, config }) {
