c878f8271244caa411929bb7d5c58a8c147efa53
Max Malm <benjick@dumfan.net>
2019-10-23 14:25:44 +0200

    Remove console.log

--- frontend/payment-klarna/hooks/afterRegistration.ts
+++ frontend/payment-klarna/hooks/afterRegistration.ts
@@ -5,7 +5,6 @@ export function afterRegistration({ Vue, isServer, config, store }) {
     const url = config.klarna.validate_order
     Vue.prototype.$bus.$on('klarna-event-order_total_change', () => {
       const {orderId} = store.state.kco.checkout
-      console.log('KLARNA UPDATE TOTAL CHANGE', url, orderId)
       if (url && orderId) {
         TaskQueue.execute({ url,
           payload: {
