42754abe7cdfe9f758f07ac68b4a7d0fb95258a6
Max Malm <benjick@dumfan.net>
2020-01-14 01:10:37 +0100

    Use const

--- packages/payment-klarna/plugins/validateOrderAmount.ts
+++ packages/payment-klarna/plugins/validateOrderAmount.ts
@@ -4,7 +4,7 @@ const plugin: KlarnaPlugin = {
   name: 'validateOrderAmount',
   fn: ({ getters }): KlarnaOrder => {
     const order: KlarnaOrder = getters.order
-    let sum = order.order_lines.reduce((acc, line) => acc + line.total_amount, 0)
+    const sum = order.order_lines.reduce((acc, line) => acc + line.total_amount, 0)
     if (order.order_amount !== sum) {
       throw new Error(`Order amount incorrect (${order.order_amount}:${sum})`)
     }
