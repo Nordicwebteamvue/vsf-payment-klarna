8bddd0a0ae4aa619f11db33cdc7a3b49f466578d
Max Malm <benjick@dumfan.net>
2020-04-02 21:46:10 +0200

    Remove KSS as a default plugin

--- packages/payment-klarna/plugins/index.ts
+++ packages/payment-klarna/plugins/index.ts
@@ -1,6 +1,5 @@
 import { KlarnaPlugin } from '../types'
 import newsletter from './newsletter'
-import kss from './kss'
 import shippingAttributes from './shippingAttributes'
 import validateOrderAmount from './validateOrderAmount'
 import lastOrder from './lastOrder'
@@ -10,7 +9,6 @@ import savedShippingMethod from './savedShippingMethod'
 export const plugins: Array<KlarnaPlugin> = []
 
 export const defaultPlugins: Array<KlarnaPlugin> = [
-  kss,
   newsletter,
   shippingAttributes,
   validateOrderAmount,
